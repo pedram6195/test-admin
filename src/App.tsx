2ce96bd61738e95cdd3e2c6f07c849a16d29c60f
Pedram Afrasiabi <p.afrasiabi@shetab.net>
2024-03-13 14:59:28 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Admin, Resource } from "react-admin";
+import { dataProvider } from "./dataProvider";
+import { UserList } from "./users";
+
+export const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={UserList} />
+  </Admin>
+);
